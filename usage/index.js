031df9675e90e969247a19a7066d960d6a8f11b0
MichaelWang <michael_wangguanyi@163.com>
2024-11-21 15:12:47 +0800

    usage demo

---
+++ usage/index.js
@@ -0,0 +1,17 @@
+
+// import {PrivateKey} from "../sdk/dist/testnet/wasm.js";
+import { Account } from "../sdk/dist/testnet/node.js";
+
+
+const account = new Account();
+
+// Individual keys can then be accessed through the following methods
+const privateKey = account.privateKey().to_string();
+const viewKey = account.viewKey().to_string();
+const address = account.address().to_string();
+
+console.log(
+    privateKey, viewKey, address
+);
+
+
